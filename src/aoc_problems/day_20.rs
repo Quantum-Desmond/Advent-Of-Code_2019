ca478be0f8d01c97ed40204d17d7a7a097832970
Karl McCarron <karlmccarron@hotmail.com>
2020-01-01 18:13:26 +0000

    Finished day 20

--- src/aoc_problems/day_20.rs
+++ src/aoc_problems/day_20.rs
@@ -9,6 +9,8 @@ use std::result;
 
 use std::collections::{BTreeMap, BTreeSet, HashMap, VecDeque};
 
+use itertools::Itertools;
+
 type Result<T> = result::Result<T, Box<dyn Error>>;
 
 macro_rules! err {
@@ -134,9 +136,11 @@ impl fmt::Display for TileType {
 struct PlutoMaze {
     tile_map: BTreeMap<Coordinate, TileType>,
     portal_map: HashMap<TileType, Vec<Coordinate>>,
-    dist_map: BTreeMap<Coordinate, usize>,
+    inside_portals: BTreeSet<Coordinate>,
+    outside_portals: BTreeSet<Coordinate>,
     starting_position: Coordinate,
     end_position: Coordinate,
+    end_distance: usize
 }
 
 impl PlutoMaze {
@@ -145,6 +149,8 @@ impl PlutoMaze {
         println!("Size of line = {:?}", chars.iter().map(|line| line.len()).collect::<Vec<_>>());
         let mut tile_map: BTreeMap<Coordinate, TileType> = BTreeMap::new();
         let mut portal_map: HashMap<TileType, Vec<Coordinate>> = HashMap::new();
+        let mut inside_portals: BTreeSet<Coordinate> = BTreeSet::new();
+        let mut outside_portals: BTreeSet<Coordinate> = BTreeSet::new();
         for (y, line) in chars.iter().enumerate() {
             for (x, &c) in line.iter().enumerate() {
                 // println!("Looking at coordinate ({}, {})", x, y);
@@ -170,13 +176,27 @@ impl PlutoMaze {
                                 }).next()
                                 .ok_or("Cannot find second portal character")?;
 
+                            // second letter coord is on the outside of the map if outside portal
+                            let is_outside_portal = vec![0, chars.len()-1].contains(&second_letter_coord.y)
+                                || vec![0, chars[second_letter_coord.y].len()-1].contains(&second_letter_coord.x);
+
                             let mut letter_coords = vec![first_letter_coord, second_letter_coord];
                             letter_coords.sort();
 
-                            let (first_char, second_char) = (chars[letter_coords[0].y][letter_coords[0].x], chars[letter_coords[1].y][letter_coords[1].x]);
+                            let char_tuple = (chars[letter_coords[0].y][letter_coords[0].x], chars[letter_coords[1].y][letter_coords[1].x]);
+                            if is_outside_portal {
+                                outside_portals.insert(Coordinate::new(x, y));
+                            } else {
+                                inside_portals.insert(Coordinate::new(x, y));
+                            }
 
-                            tile_map.insert(Coordinate::new(x, y), TileType::Portal((first_char, second_char)));
-                            let portal_entry = portal_map.entry(TileType::Portal((first_char, second_char))).or_insert(vec![]);
+                            let mut letter_coords = vec![first_letter_coord, second_letter_coord];
+                            letter_coords.sort();
+
+                            let char_tuple = (chars[letter_coords[0].y][letter_coords[0].x], chars[letter_coords[1].y][letter_coords[1].x]);
+
+                            tile_map.insert(Coordinate::new(x, y), TileType::Portal(char_tuple));
+                            let portal_entry = portal_map.entry(TileType::Portal(char_tuple)).or_insert(vec![]);
                             portal_entry.push(Coordinate::new(x, y));
                         } else {
                             tile_map.insert(Coordinate::new(x, y), TileType::Open);
@@ -207,62 +227,143 @@ impl PlutoMaze {
             PlutoMaze {
                 tile_map,
                 portal_map,
-                dist_map: BTreeMap::new(),
+                outside_portals,
+                inside_portals,
                 starting_position,
-                end_position
+                end_position,
+                end_distance: 0
             }
         )
     }
 
-    fn adjacent_tiles(&self, coord: Coordinate) -> Result<Vec<Coordinate>> {
-        let neighbours: Vec<Coordinate> = match self.tile_map.get(&coord).ok_or(format!("Cannot find {} in tile map", coord))? {
+    fn adjacent_tiles(&self, position: (Coordinate, usize), recursive: bool) -> Result<Vec<(Coordinate, usize)>> {
+        let (coord, recursion_level) = position;
+        let neighbours: Vec<(Coordinate, usize)> = match self.tile_map.get(&coord).ok_or(format!("Cannot find {} in tile map", coord))? {
             TileType::Open => {
                 coord.neighbours()
                     .into_iter()
-                    .filter(|coord| self.tile_map.get(&coord) != Some(&TileType::Wall) && self.tile_map.get(&coord) != Some(&TileType::Blank))
+                    .filter_map(|coord| {
+                        if self.tile_map.get(&coord) != Some(&TileType::Wall) && self.tile_map.get(&coord) != Some(&TileType::Blank) {
+                            Some((coord, recursion_level))
+                        } else {
+                            None
+                        }
+                    })
                     .collect()
             },
             TileType::Portal(char_tuple) => {
-                let mut neighbours: Vec<_> = coord.neighbours()
+                let mut neighbours = coord.neighbours()
                     .into_iter()
-                    .filter(|coord| self.tile_map.get(&coord) != Some(&TileType::Wall) && self.tile_map.get(&coord) != Some(&TileType::Blank))
-                    .collect();
-                if *char_tuple != ('A', 'A') && *char_tuple != ('Z', 'Z') {
-                    let other_portal_coord: Coordinate = self.portal_map.get(&TileType::Portal(*char_tuple))
-                        .ok_or(format!("Cannot find portal for {:?}", char_tuple))?
-                        .iter()
-                        .filter(|&&cc| cc != coord)
-                        .next()
-                        .ok_or(format!("Cannot find other portal coordinate for {}", coord))?
-                        .clone();
-                    neighbours.push(other_portal_coord);
+                    .filter_map(|coord| {
+                        if self.tile_map.get(&coord) != Some(&TileType::Wall) && self.tile_map.get(&coord) != Some(&TileType::Blank) {
+                            Some((coord, recursion_level))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect_vec();
+                if recursive {
+                    // if level = 0, only outside portals = AA, ZZ
+                    // otherwise,
+                    //     - inside goes to outside with recursion level + 1
+                    //     - outside goes to inside with recursion level - 1
+                    if self.inside_portals.contains(&coord) {
+                        let other_portal_coord: Coordinate = self.portal_map.get(&TileType::Portal(*char_tuple))
+                            .ok_or(format!("Cannot find portal for {:?}", char_tuple))?
+                            .iter()
+                            .filter(|&&cc| cc != coord)
+                            .next()
+                            .ok_or(format!("Cannot find other portal coordinate for inside portal {}", coord))?
+                            .clone();
+                        neighbours.push((other_portal_coord, recursion_level+1));
+                    } else if self.outside_portals.contains(&coord) {
+                        if recursion_level != 0 && *char_tuple != ('A', 'A') && *char_tuple != ('Z', 'Z') {
+                            let other_portal_coord: Coordinate = self.portal_map.get(&TileType::Portal(*char_tuple))
+                                .ok_or(format!("Cannot find portal for {:?}", char_tuple))?
+                                .iter()
+                                .filter(|&&cc| cc != coord)
+                                .next()
+                                .ok_or(format!("Cannot find other portal coordinate for outside portal {}", coord))?
+                                .clone();
+                            neighbours.push((other_portal_coord, recursion_level-1));
+                        }
+                    } else {
+                        return err!("{} is neither inside or outside portal", coord);
+                    }
+                } else {
+                    if *char_tuple != ('A', 'A') && *char_tuple != ('Z', 'Z') {
+                        let other_portal_coord: Coordinate = self.portal_map.get(&TileType::Portal(*char_tuple))
+                            .ok_or(format!("Cannot find portal for {:?}", char_tuple))?
+                            .iter()
+                            .filter(|&&cc| cc != coord)
+                            .next()
+                            .ok_or(format!("Cannot find other portal coordinate for {}", coord))?
+                            .clone();
+                        neighbours.push((other_portal_coord, recursion_level));
+                    }
                 }
+
                 neighbours
             },
             tile_type => return err!("Cannot find neighbours of type {:?}", tile_type),
         };
 
         Ok(neighbours)
+        // let neighbours: Vec<Coordinate> = match self.tile_map.get(&coord).ok_or(format!("Cannot find {} in tile map", coord))? {
+        //     TileType::Open => {
+        //         coord.neighbours()
+        //             .into_iter()
+        //             .filter(|coord| {
+        //                 if recursive {
+
+        //                 } else {
+        //                     self.tile_map.get(&coord) != Some(&TileType::Wall) && self.tile_map.get(&coord) != Some(&TileType::Blank)
+        //                 }
+        //             })
+        //             .collect()
+        //     },
+        //     TileType::Portal(char_tuple) => {
+        //         let mut neighbours: Vec<_> = coord.neighbours()
+        //             .into_iter()
+        //             .filter(|coord| self.tile_map.get(&coord) != Some(&TileType::Wall) && self.tile_map.get(&coord) != Some(&TileType::Blank))
+        //             .collect();
+        //         if *char_tuple != ('A', 'A') && *char_tuple != ('Z', 'Z') {
+        //             let other_portal_coord: Coordinate = self.portal_map.get(&TileType::Portal(*char_tuple))
+        //                 .ok_or(format!("Cannot find portal for {:?}", char_tuple))?
+        //                 .iter()
+        //                 .filter(|&&cc| cc != coord)
+        //                 .next()
+        //                 .ok_or(format!("Cannot find other portal coordinate for {}", coord))?
+        //                 .clone();
+        //             neighbours.push(other_portal_coord);
+        //         }
+        //         neighbours
+        //     },
+        //     tile_type => return err!("Cannot find neighbours of type {:?}", tile_type),
+        // };
+
+        // Ok(neighbours)
     }
 
-    fn find_path_through_maze(&mut self) -> Result<()> {
-        self.dist_map.insert(self.starting_position, 0);
+    fn find_path_through_maze(&mut self, recursive: bool) -> Result<()> {
+        let mut d = BTreeMap::new();
+        d.insert((self.starting_position, 0), 0);
 
-        let mut queue: VecDeque<Coordinate> = VecDeque::new();
-        queue.push_front(self.starting_position);
+        let mut queue: VecDeque<(Coordinate, usize)> = VecDeque::new();
+        queue.push_front((self.starting_position, 0));
 
-        let mut todo_set: BTreeSet<Coordinate> = BTreeSet::new();
-        let mut visited: BTreeSet<Coordinate> = BTreeSet::new();
+        let mut todo_set: BTreeSet<(Coordinate, usize)> = BTreeSet::new();
+        let mut visited: BTreeSet<(Coordinate, usize)> = BTreeSet::new();
 
         while let Some(c) = queue.pop_front() {
             todo_set.remove(&c);
             visited.insert(c);
 
-            if c == self.end_position {
+            if c == (self.end_position, 0) {
                 break;
             }
 
-            for neighbour in self.adjacent_tiles(c)? {
+            for neighbour in self.adjacent_tiles(c, recursive)? {
                 // Don't add to squares to do
                 if visited.contains(&neighbour) {
                     continue;
@@ -273,19 +374,16 @@ impl PlutoMaze {
                     todo_set.insert(neighbour);
                 }
 
-                let new_dist = 1 + *self.dist_map.get(&c).unwrap_or(&0);
-                if !self.dist_map.contains_key(&neighbour) || new_dist < self.dist_map[&neighbour] {
-                    self.dist_map.insert(neighbour, new_dist);
+                let new_dist = 1 + *d.get(&c).unwrap_or(&0);
+                if !d.contains_key(&neighbour) || new_dist < d[&neighbour] {
+                    d.insert(neighbour, new_dist);
                 }
             }
         }
+
+        self.end_distance = *d.get(&(self.end_position, 0)).ok_or("End position not in distance map")?;
+
         Ok(())
-    }
-
-    fn shortest_path_through_maze(&self) -> Result<usize> {
-        let dist = self.dist_map.get(&self.end_position).ok_or("End position not in distance map")?;
-
-        Ok(*dist)
     }
 }
 
@@ -320,8 +418,8 @@ fn _q1(chars: Vec<Vec<char>>) -> Result<usize> {
     let mut maze = PlutoMaze::new(chars)?;
     println!("{}", maze);
     println!("Created maze object");
-    maze.find_path_through_maze()?;
-    maze.shortest_path_through_maze()
+    maze.find_path_through_maze(false)?;
+    Ok(maze.end_distance)
 }
 
 pub fn q2(fname: String) -> usize {
@@ -330,13 +428,16 @@ pub fn q2(fname: String) -> usize {
 
     f.read_to_string(&mut f_contents).expect("Couldn't find file");
 
-    let map_char_list: Vec<Vec<char>> = f_contents.trim().split('\n').map(|s| s.trim().chars().collect()).collect();
+    let chars: Vec<Vec<char>> = f_contents.split('\n').filter(|s| s.len() > 0).map(|s| s.chars().collect()).collect();
 
-    _q2(map_char_list).unwrap()
+    _q2(chars).unwrap()
 }
 
-fn _q2(_chars: Vec<Vec<char>>) -> Result<usize> {
-    unimplemented!();
+fn _q2(chars: Vec<Vec<char>>) -> Result<usize> {
+    let mut maze = PlutoMaze::new(chars)?;
+    println!("Created maze object");
+    maze.find_path_through_maze(true)?;
+    Ok(maze.end_distance)
 }
 
 #[cfg(test)]
@@ -418,6 +519,54 @@ YN......#               VT..#....QG
         assert_eq!(
             _q1(map).unwrap(),
             58
+        )
+    }
+
+    #[test]
+    fn day20_q2_test() {
+        let map: Vec<Vec<char>> = "
+             Z L X W       C                 
+             Z P Q B       K                 
+  ###########.#.#.#.#######.###############  
+  #...#.......#.#.......#.#.......#.#.#...#  
+  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  
+  #.#...#.#.#...#.#.#...#...#...#.#.......#  
+  #.###.#######.###.###.#.###.###.#.#######  
+  #...#.......#.#...#...#.............#...#  
+  #.#########.#######.#.#######.#######.###  
+  #...#.#    F       R I       Z    #.#.#.#  
+  #.###.#    D       E C       H    #.#.#.#  
+  #.#...#                           #...#.#  
+  #.###.#                           #.###.#  
+  #.#....OA                       WB..#.#..ZH
+  #.###.#                           #.#.#.#  
+CJ......#                           #.....#  
+  #######                           #######  
+  #.#....CK                         #......IC
+  #.###.#                           #.###.#  
+  #.....#                           #...#.#  
+  ###.###                           #.#.#.#  
+XF....#.#                         RF..#.#.#  
+  #####.#                           #######  
+  #......CJ                       NM..#...#  
+  ###.#.#                           #.###.#  
+RE....#.#                           #......RF
+  ###.###        X   X       L      #.#.#.#  
+  #.....#        F   Q       P      #.#.#.#  
+  ###.###########.###.#######.#########.###  
+  #.....#...#.....#.......#...#.....#.#...#  
+  #####.#.###.#######.#######.###.###.#.#.#  
+  #.......#.......#.#.#.#.#...#...#...#.#.#  
+  #####.###.#####.#.#.#.#.###.###.#.###.###  
+  #.......#.....#.#...#...............#...#  
+  #############.#.#.###.###################  
+               A O F   N                     
+               A A D   M                     
+        ".split('\n').filter(|&line| line.trim().len() > 0).map(|line| line.chars().collect()).collect();
+
+        assert_eq!(
+            _q2(map).unwrap(),
+            396
         )
     }
 }
